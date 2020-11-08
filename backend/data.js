ef0170e32890cf91f1f1e988ccdd88678b6ca85f
Alejogb1 <magnicetio@gmail.com>
2020-11-08 14:23:33 -0300

    .

---
+++ backend/data.js
@@ -0,0 +1,33 @@
+export default {
+    products: [{
+        _id: "1",
+        name: "Parmesan Cheese",
+        image: "/img/parmesan.jpg",
+        price: 120,
+        brand: "Vaquero Quesos",
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6
+    },
+    {
+        _id: "3",
+        name: "Gruyere Cheese",
+        image: "/img/gruyere.jpg",
+        price: 100,
+        brand: "Santa Fe",
+        rating: 4.2,
+        numReviews: 40,
+        countInStock: 3
+    },
+    {   
+        _id: "2",
+        name: "Blue Cheese",
+        image: "/img/blue.jpg",
+        price: 60,
+        brand: "Cordoba Quesos",
+        rating: 2.5,
+        numReviews: 20,
+        countInStock: 15
+    }
+]
+}
