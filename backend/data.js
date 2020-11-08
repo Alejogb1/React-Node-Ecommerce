b3a4405313055139dd41e9fea988b3641cc1e2fe
Alejogb1 <magnicetio@gmail.com>
2020-11-08 18:45:48 -0300

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -2,31 +2,37 @@ export default {
     products: [{
         _id: "1",
         name: "Parmesan Cheese",
+        description: "Generic parmesan cheese is a family of hard grating cheeses made from cow's milk and inspired by the original Italian cheese. They are generally pale yellow in color, and usually used grated on dishes like spaghetti, Caesar salad, and pizza. American generic parmesan is frequently sold already grated and has been aged for less than 12 months.",
         image: "/img/parmesan.jpg",
         price: 120,
         brand: "Vaquero Quesos",
         rating: 4.5,
         numReviews: 10,
+        stars: "⭐⭐⭐⭐⭐",
         countInStock: 6
     },
     {
         _id: "3",
         name: "Gruyere Cheese",
+        description:"Gruyère cheese is generally known as one of the finest cheeses for baking, having a distinctive but not overpowering taste. In quiche, Gruyère adds savoriness without overshadowing the other ingredients. It is a good melting cheese, particularly suited for fondues, along with Vacherin Fribourgeois and Emmental. It is also traditionally used in French onion soup, as well as in croque-monsieur, a classic French toasted ham and cheese sandwich. ",
         image: "/img/gruyere.jpg",
         price: 100,
         brand: "Santa Fe",
         rating: 4.2,
         numReviews: 40,
+        stars: "⭐⭐⭐",
         countInStock: 3
     },
     {   
         _id: "2",
         name: "Blue Cheese",
+        description: "Blue cheese or bleu cheese is cheese made with cultures of the mold Penicillium, giving it spots or veins of the mold throughout the cheese, which can vary in color through various shades of blue and green. This carries a distinct smell, either from that or various specially cultivated bacteria. Some blue cheeses are injected with spores before the curds form, and others have spores mixed in with the curds after they form.",
         image: "/img/blue.jpg",
         price: 60,
         brand: "Cordoba Quesos",
         rating: 2.5,
         numReviews: 20,
+        stars: "⭐⭐⭐⭐",
         countInStock: 15
     }
 ]
